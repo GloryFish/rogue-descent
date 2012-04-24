63ce7911e1b5da1e793335fbc79af9734bece380
Jay Roberts <jay@gloryfish.org>
2012-04-23 23:33:53 -0400

    Add animations.lua

---
+++ loveapp/resources/sprites/animations.lua
@@ -0,0 +1,148 @@
+local animations = {
+
+  player_man_standing = {
+    frames = {
+      {
+        name = 'player_man_standing', -- this is a Zwoptex sprite name
+        duration = 1.0 -- duration in seconds
+      },
+      -- Add new frames here
+    }
+  },
+  player_man_walking = {
+    frames = {
+      {
+        name = 'player_man_walk_1', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_man_walk_2', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_man_walk_3', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_man_walk_4', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_man_walk_5', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_man_walk_6', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      -- Add new frames here
+    }
+  },  -- Add new animations here
+  
+  player_woman_standing = {
+    frames = {
+      {
+        name = 'player_woman_standing', -- this is a Zwoptex sprite name
+        duration = 1.0 -- duration in seconds
+      },
+      -- Add new frames here
+    }
+  },  
+  
+  player_woman_walking = {
+    frames = {
+      {
+        name = 'player_woman_walk_1', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_woman_walk_2', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_woman_walk_3', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_woman_walk_4', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_woman_walk_5', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'player_woman_walk_6', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      -- Add new frames here
+    }
+  },  -- Add new animations here
+  slime_blue_bouncing = {
+    frames = {
+      {
+        name = 'slime_blue_1', -- this is a Zwoptex sprite name
+        duration = 0.1 -- duration in seconds
+      },
+      {
+        name = 'slime_blue_2', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'slime_blue_1', -- this is a Zwoptex sprite name
+        duration = 0.1 -- duration in seconds
+      },
+      {
+        name = 'slime_blue_3', -- this is a Zwoptex sprite name
+        duration = 0.1 -- duration in seconds
+      },
+      -- Add new frames here
+    }
+  },
+  slime_red_bouncing = {
+    frames = {
+      {
+        name = 'slime_red_1', -- this is a Zwoptex sprite name
+        duration = 0.2 -- duration in seconds
+      },
+      {
+        name = 'slime_red_2', -- this is a Zwoptex sprite name
+        duration = 0.4 -- duration in seconds
+      },
+      {
+        name = 'slime_red_1', -- this is a Zwoptex sprite name
+        duration = 0.15 -- duration in seconds
+      },
+      {
+        name = 'slime_red_3', -- this is a Zwoptex sprite name
+        duration = 0.15 -- duration in seconds
+      },
+      -- Add new frames here
+    }
+  },
+  slime_green_bouncing = {
+    frames = {
+      {
+        name = 'slime_green_1', -- this is a Zwoptex sprite name
+        duration = 0.1 -- duration in seconds
+      },
+      {
+        name = 'slime_green_2', -- this is a Zwoptex sprite name
+        duration = 0.25 -- duration in seconds
+      },
+      {
+        name = 'slime_green_1', -- this is a Zwoptex sprite name
+        duration = 0.13 -- duration in seconds
+      },
+      {
+        name = 'slime_green_3', -- this is a Zwoptex sprite name
+        duration = 0.1 -- duration in seconds
+      },
+      -- Add new frames here
+    }
+  },
+}
+
+
+
+return animations
