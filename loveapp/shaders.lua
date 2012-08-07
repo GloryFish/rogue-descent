a056d30928551a342a0e31d4a906c21400e1d415
Jay Roberts <jay@gloryfish.org>
2012-08-07 00:27:15 -0400

    Fix whitespace.

--- loveapp/shaders.lua
+++ loveapp/shaders.lua
@@ -1,10 +1,10 @@
--- 
+--
 --  shaders.lua
 --  rogue-descent
---  
+--
 --  Created by Jay Roberts on 2012-04-27.
 --  Copyright 2012 GloryFish.org. All rights reserved.
--- 
+--
 
 require 'middleclass'
 
@@ -13,7 +13,7 @@ local Shaders = class('Shaders')
 function Shaders:initialize()
   self.canvas = love.graphics.newCanvas()
   self.quad = love.graphics.newQuad(0, 0, love.graphics.getWidth(), love.graphics.getHeight(), love.graphics.getWidth(), love.graphics.getHeight())
-  
+
   -- Tweakable parameters
   self.focus = vector(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
 
@@ -22,7 +22,7 @@ end
 
 function Shaders:reload()
   self.shaders = {}
-  
+
   local filelist = love.filesystem.enumerate('effects')
 
   local count = 0
@@ -31,9 +31,9 @@ function Shaders:reload()
     local shader = {
       fragment = love.filesystem.read('effects/'..filename)
     }
-    
+
     local success, effect = pcall(love.graphics.newPixelEffect, shader.fragment)
-		
+
     if success then
       shader.effect = effect
       shader.externs = {}
@@ -41,14 +41,14 @@ function Shaders:reload()
       for type, name in shader.fragment:gmatch("extern (%w+) (%w+)") do
         table.insert(shader.externs, name)
       end
-      
+
 			self.shaders[filename:sub(1, -6)] = shader
       count = count + 1
 		else
 			print(string.format("Error loading shader (%s):\n", filename), effect)
 		end
   end
-  
+
   print('Loaded '..count..' effects')
 end
 
@@ -68,21 +68,22 @@ function Shaders:set(name)
     for i, extern in ipairs(shader.externs) do
       if extern == 'time' then
         shader.effect:send('time', love.timer.getTime())
-        
+
       elseif extern == 'textureSize' then
         shader.effect:send('textureSize', {love.graphics.getWidth(), love.graphics.getHeight()})
 
       elseif extern == 'inputSize' then
         shader.effect:send('inputSize', {love.graphics.getWidth(), love.graphics.getHeight()})
-        
+
       elseif extern == 'outputSize' then
         shader.effect:send('outputSize', {love.graphics.getWidth(), love.graphics.getHeight()})
-        
+
       elseif extern == 'focus' then
         shader.effect:send('focus', {self.focus:unpack()})
+
       end
     end
-    
+
     love.graphics.setPixelEffect(self.shaders[name].effect)
   else
     love.graphics.setPixelEffect()
